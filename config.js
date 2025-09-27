e08de4ca9f77a13f066c91d3a046c4e829ff210c
dmazzacan-byte <dmazzacan@gmail.com>
2025-09-26 22:02:52 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+export const clientConfigs = {
+  "operis-1": {
+    displayName: "Operis 1",
+    firebaseConfig: {
+      apiKey: "AIzaSyAyMsDnA4TadOXrwxUqumwPAji9S3QiEAE",
+      authDomain: "superprod-2ced1.firebaseapp.com",
+      projectId: "superprod-2ced1",
+      storageBucket: "superprod-2ced1.appspot.com",
+      messagingSenderId: "691324529613",
+      appId: "1:691324529613:web:a050a6d44f06481503b284",
+      measurementId: "G-53FH6JGS20"
+    }
+  },
+  "operis-2": {
+    displayName: "Operis 2",
+    firebaseConfig: {
+      apiKey: "AIzaSyAJ5RsHYQLmwfFNDE6lsiTjXm9uhXHFvOg",
+      authDomain: "operis-2.firebaseapp.com",
+      projectId: "operis-2",
+      storageBucket: "operis-2.firebasestorage.app",
+      messagingSenderId: "558948066489",
+      appId: "1:558948066489:web:9eaf8a0b4fa8d04a676768",
+      measurementId: "G-DGXEJMFV46"
+    }
+  }
+};
